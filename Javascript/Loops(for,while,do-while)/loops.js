382e8f5efd5802711fcb19817565976503646abc
shivshankar646 <shivshankarmunde91@gmail.com>
2025-09-15 20:42:52 +0530

    loops

---
+++ Javascript/Loops(for,while,do-while)/loops.js
@@ -0,0 +1,180 @@
+    
+
+
+//1 to 10 using three loops
+// for (let i = 1; i <=10; i++) {
+//     console.log(i)
+    
+// }
+
+// let i=1;
+// while (i<=10) {
+//     console.log(i);
+//     i++;
+// }
+// let j=1;
+// do {
+//     console.log(j);
+//     j++;
+// } while (j <= 10);
+
+// 5 table
+// for (let i = 1; i <=10; i++) {
+//     console.log(5 ,'x',i , '=', 5*i)
+    
+// }
+// let i=1;
+// while (i<=10) {
+//     console.log(5,'x',i,'=',5*i);
+//     i++;
+// }
+
+// let j=1;
+// do {
+//     console.log(5,'x',j,'=',5*j);
+//     j++;
+// } while (j<=10);
+
+//find the sum of 1 to 100 using three loops
+
+// let c=0;
+// for (let i = 1; i <=100; i++) {
+//     c+=i;
+// }
+// console.log(c)
+
+// let i=1;
+// while (i<=100) {
+//     c+=i;
+//     console.log(c);
+//     i++;
+// }
+
+// let i=1;
+// do {
+//     c+=i;
+//     console.log(c)
+//     i++;
+// } while (i<=100);
+
+//print number from 10 to 1
+
+// for (let i = 10; i >=1; i--) {
+   
+//     console.log(i);
+// }
+
+// let i=10;
+// while (i>=1) {
+//     console.log(i);
+//     i--;
+// }
+// let i=10;
+// do {
+//     console.log(i);
+//     i--;
+// } while (i>=1);
+
+//print even numbers from 1 to 20
+
+// for (let i = 1; i <=20; i++) {
+// if (i%2==0) {
+//     console.log(i);
+// }
+// }
+
+// let i=1;
+// while (i<=20) {
+//     if (i%2==0) {
+//         console.log(i)
+//     }
+//     i++;
+// }
+// let i=1;
+// do {
+//     if (i%2==0) {
+//        console.log(i); 
+//     }
+//     i++;
+// } while (i<=20);
+
+//factorial of number
+// let num = 1;
+// let number = 5;
+
+// for (let i = number; i >= 1; i--) {
+//    num *= i;
+// }
+
+// console.log("factorial of", number, "=", num);
+
+// let num = 1;
+// let number = 5;
+// let steps="";
+
+// for (let i = 1; i <=1; i--) {
+//    num *= i;
+//    steps +=i;
+//    if (i > 1) steps +="x";
+// }
+
+// console.log(number +"!="+steps+"="+num);
+
+//sum number from 1 to 100
+
+// let sum=0;
+// for (let i = 1; i <= 100; i++) {
+    
+//     if (i%2==0) {
+//         sum+=i;
+//     }
+// }
+// console.log(sum);
+
+//print the triangle pattern 
+// for (let i = 1; i <= 5; i++) {
+//    console.log("*".repeat(i))
+ 
+    
+// }
+
+// for (let i = 1; i <= 5; i++) {
+//     let row = "";
+//     for (let j = 1; j <= i; j++) {
+//         row += "*";
+//     }
+//     console.log(row);
+// }
+
+// for (let i = 5; i >= 1; i--) {
+//   let row= "";
+//   for (let j = 1; j <= i; j++) {
+//     row +="*";
+    
+//     console.log(row)
+//   }
+// }
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i%2!==0) {
+        
+//         console.log(i)
+//     }
+    
+// }
+// for (let i = 1; i <= 50; i++) {
+//     if (i%5==0) {
+        
+//         console.log(i)
+//     }
+    
+// }
+for (let i = 1; i <= 10; i++) {
+   
+    console.log(i*i)
+}
+
+for (let i = 20; i >=1 ; i--) {
+   console.log(i)
+    
+}
