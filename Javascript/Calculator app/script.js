affea8d7ef73acf0cb9f678dd09c0ce0a534d1a8
shivshankar646 <shivshankarmunde91@gmail.com>
2025-09-19 19:14:00 +0530

    calculator app

---
+++ Javascript/Calculator app/script.js
@@ -0,0 +1,27 @@
+
+function shownumber(num){
+document.getElementById("display").value += num;
+console.log(num);
+switch (key) {
+    case "+" ,num+num:
+        
+        break;
+
+    default:
+        break;
+}
+
+}
+
+function showsine(sine){
+    document.getElementById("display").value +=sine;
+    console.log(sine);
+}
+function clear(C){
+   
+    console.log(key)
+    
+}
+
+function showequal(){
+}
