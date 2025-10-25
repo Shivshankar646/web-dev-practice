6663e8bffd2e109ed56ed8500c70dba57370b2b1
Shivshankar Munde <shivshankarmunde91@gmail.com>
2025-10-25 17:54:51 +0530

    started learning react

---
+++ React/Intro to React, create app with ViteCRA, JSX basics/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+function App() {
+  return (
+    <>
+      <h1>Shivshankar  + Jay  🌍</h1>
+      <p>My first React project using Vite!</p>
+      <button>Click me</button>
+    </>
+  )
+}
+
+
+
+export default App
