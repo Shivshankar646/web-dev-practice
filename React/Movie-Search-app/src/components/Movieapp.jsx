fafee440bae8a2fd405cc09f8fda68bd8d6994e1
Shivshankar Munde <shivshankarmunde91@gmail.com>
2025-11-15 09:18:20 +0530

    changed title

--- React/Movie-Search-app/src/components/Movieapp.jsx
+++ React/Movie-Search-app/src/components/Movieapp.jsx
@@ -109,13 +109,13 @@ const MovieRecommendations = () => {
             <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
             <h2>{movie.title}</h2>
             <p className='rating'>Rating: {movie.vote_average}</p>
-            {expandedMovieId === movie.id ? (
+          {expandedMovieId === movie.id ? (
               <p>{movie.overview}</p>
             ) : (
               <p>{movie.overview.substring(0, 150)}...</p>
             )}
             <button onClick={() => toggleDescription(movie.id)} className='read-more'>
-              {expandedMovieId === movie.id ? 'Show Less' : 'Read More'}
+          {expandedMovieId === movie.id ? 'Show Less' : 'Read More'}
             </button>
           </div>
         ))}
