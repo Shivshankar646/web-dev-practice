d242b3fb949c77ab7bd17c39fc7e5e5f43490f8c
Shivshankar Munde <shivshankarmunde91@gmail.com>
2025-11-19 13:53:27 +0530

    frontend is ready

--- React/Ecommerce-App/frontend/src/App.jsx
+++ React/Ecommerce-App/frontend/src/App.jsx
@@ -10,6 +10,7 @@ import Login from './pages/Login'
 import Placeorder from './pages/Placeorder'
 import Orders from './pages/Orders'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
 <Route path='/place-order' element={<Placeorder/>} />
 <Route path='/orders' element={<Orders/>}/>
       </Routes>
+      <Footer/>
     </div>
   )
 }
