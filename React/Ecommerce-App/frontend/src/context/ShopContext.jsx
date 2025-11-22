fc47b915dc0ce9e21dc647ac5358caf802bd8c32
Shivshankar Munde <shivshankarmunde91@gmail.com>
2025-11-22 15:50:41 +0530

    builded suggestion cloths display according to category selected

--- React/Ecommerce-App/frontend/src/context/ShopContext.jsx
+++ React/Ecommerce-App/frontend/src/context/ShopContext.jsx
@@ -8,6 +8,25 @@ const currency ='$';
 const delivery_fee=10;
 const [search,setSearch] = useState('');
 const [showSearch,setShowSearch] = useState(false);
+const [cartItems,setCartItems] =useState({});
+
+const addToCart = async (itemId,size) => {
+let cartData =structuredClone(cartItems);
+if (cartData[itemId]) {
+  if (cartData[itemId][size]) {
+    cartData[itemId][size] += 1; 
+  }
+   else{
+    cartData[itemId][size] = 1;
+   }
+}
+else{
+  cartData[itemId] = {};
+  cartData[itemId] = 1;
+}
+
+}
+
 
 const value ={
   products,currency,delivery_fee,
