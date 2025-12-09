4673163c24a95f77e2564cc13e7ded050dfb06a3
Shivshankar Munde <shivshankarmunde91@gmail.com>
2025-12-09 16:42:51 +0530

    learned put,post,delete api using thunder and postman

--- Node/REST APIs in Express/app.js
+++ Node/REST APIs in Express/app.js
@@ -8,9 +8,38 @@ let users =[
         id:2,name:'bro'
     }
 ];
+app.post('/users',(req,res)=>{
+    const newuser =req.body;
+    users.push(newuser);
+    res.status(201).json(newuser)
+})
+;
 app.get('/users',(req,res)=>{
     res.json(users);
-})
+    console.log(users)
+});
+
+app.put('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    const updatedData = req.body;
+
+    users = users.map(user =>
+        user.id === userId ? updatedData : user
+    );
+
+    res.json({ message: 'User updated', updatedData });
+    console.log(users);
+
+});
+
+app.delete('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    users = users.filter(user => user.id !== userId);
+    res.json({ message: 'User removed' });
+    console.log(users)
+});
+
+
 
 app.listen(port,()=>{
     console.log(`server is running on http://localhost:${port}`)
