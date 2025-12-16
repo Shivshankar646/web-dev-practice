f96f9f7548eb03e11537e4f963e56b2de3b7e2d0
Shivshankar Munde <shivshankarmunde91@gmail.com>
2025-12-16 08:58:57 +0530

    today learned about rest apis in monodb

--- Node/notes-app/server.js
+++ Node/notes-app/server.js
@@ -14,5 +14,5 @@ app.use(express.json());
 app.use(noteRoutes);
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
