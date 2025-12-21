edefa667f1deaed6e6072a59b2214516e5851def
Shivshankar Munde <shivshankarmunde91@gmail.com>
2025-12-21 09:35:31 +0530

    i have learned crud today

---
+++ Node/Logic building-4/index.js
@@ -0,0 +1,26 @@
+let notes = [
+  { bhai:"Shiv",id: 1, title: "first" },
+  { id: 2, title: "second" }
+];
+
+function getNotes(notes) {
+return notes;
+};
+
+
+function createNote(notes, title) {
+const newnote ={id:notes.length + 1,title};
+return [newnote,...notes]
+};
+
+
+function updateNote(notes, id, newTitle) {
+return notes.map(note => note.id === id ? {...note,id,newTitle} : note)
+};
+console.log(updateNote(notes,1,"i am god in coding"))
+
+function deleteNote(notes, id) {
+return notes.filter(note => note.id !== id);
+};
+
+
