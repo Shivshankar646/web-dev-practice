db094b1a4d924c6573cbd35a13c838b452d08d22
Shivshankar Munde <shivshankarmunde91@gmail.com>
2026-01-01 09:18:15 +0530

    i have updated expense tracker

--- Projects/EXPENSE TRACKER/index.js
+++ Projects/EXPENSE TRACKER/index.js
@@ -1,20 +1,36 @@
-let arr = [];
-
+let arr =JSON.parse(localStorage.getItem('user')) || [];
+let title =document.getElementById("title");
 let add =document.getElementById("add");
-add.addEventListener("click",()=>{
-let title = document.getElementById("title");
 let amount = document.getElementById("amount");
 let incexp = document.getElementById("incexp");
+add.disabled=true;
+function validateInputs() {
+
+  add.disabled =
+    title.value.trim() === "" ||
+    amount.value.trim() === "" ||
+    incexp.value === "";
+   
+
+}
+
+title.addEventListener("input", validateInputs);
+amount.addEventListener("input", validateInputs);
+incexp.addEventListener("change", validateInputs);
+
+add.addEventListener("click",()=>{
+  if (Number(amount.value) <= 0) return;
+
 arr.push({id:Date.now(),title:title.value,amount:amount.value,type:incexp.value});
 title.value= "";
 amount.value="";
-incexp.value="";
-
 render();
 totalcalculator();
+add.disabled=true;
 });
 
 function render(){
+  localStorage.setItem('user',JSON.stringify(arr));
     let display = document.getElementById("display");
     display.innerHTML= "";
 
@@ -30,7 +46,9 @@ btn.addEventListener("click",(e) =>{
     arr =arr.filter(item => item.id !== id);
     totalcalculator()
     render();
-})
+});
+li.classList.add(item.type); // income / expense
+
 li.innerText =`${item.title}-${item.amount}Rs.-${item.type} `;
 li.appendChild(btn);
 display.appendChild(li)
@@ -39,7 +57,7 @@ display.appendChild(li)
 
 function totalcalculator() {
   let Total = document.getElementById("Total");
-
+console.log(arr)
   let balance = arr.reduce((sum, item) => {
     if (item.type === "income") {
       return sum + Number(item.amount);
@@ -50,3 +68,5 @@ function totalcalculator() {
 
   Total.innerText = `Total balance: ${balance} Rs`;
 }
+render();
+totalcalculator();
