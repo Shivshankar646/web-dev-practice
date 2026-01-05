71293f4987684844908b813ab826a2ba8e4cc635
Shivshankar Munde <shivshankarmunde91@gmail.com>
2026-01-05 13:27:40 +0530

    i have started

--- Projects/expense-tracker-react/src/components/ExpenseList.jsx
+++ Projects/expense-tracker-react/src/components/ExpenseList.jsx
@@ -6,7 +6,7 @@ const ExpenseList = ({expenses}) => {
     {expenses.map((item) => (
       <li key={item.id}>
         {item.title} - {item.amount} - {item.type}
-        <button   onClick={() => toggleDone(!item.done)} id='done' style={{backgroundColor:item.done ? "green" : "red"}}>Done</button>
+        <button   onClick={() => toggleDone(item.id)} id='done' style={{backgroundColor:item.done ? "green" : "red"}}>Done</button>
         <button>Delete</button>
       </li>
     ))}
