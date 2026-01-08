d7bab13c7ac3a204d026e6452ddfb3119b2858e8
Shivshankar Munde <shivshankarmunde91@gmail.com>
2026-01-08 13:52:46 +0530

    used local storage

--- Projects/expense-tracker-react/src/App.jsx
+++ Projects/expense-tracker-react/src/App.jsx
@@ -1,33 +1,47 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ExpenseForm from "./components/ExpenseForm";
 import ExpenseList from "./components/ExpenseList";
 import Balance from "./components/Balance";
 
 function App() {
-  const [expenses, setExpenses] = useState([]);
+  const storedvalue = JSON.parse(localStorage.getItem("bhai")) || [];
+  const [expenses, setExpenses] = useState(storedvalue);
+
   function addExpense(expense) {
     setExpenses((prev) => [...prev, expense]);
   }
-function toggleDone(id){
-  setExpenses(prev => prev.map(item => item.id === id ? {...item,done:!item.done} : item))
-}
 
-function toggleDelete(id) {
-  setExpenses(prev => prev.filter(item => item.id !== id))
-}
+  function toggleDone(id) {
+    setExpenses((prev) =>
+      prev.map((item) =>
+        item.id === id ? { ...item, done: !item.done } : item
+      )
+    );
+  }
 
- const balance = expenses.reduce((total,item) => {
-  return item.type === "income" 
-  ? total + item.amount
-   : total - item.amount;
- },0)
+  function toggleDelete(id) {
+    setExpenses((prev) => prev.filter((item) => item.id !== id));
+  }
+
+  useEffect(() => {
+    localStorage.setItem("bhai", JSON.stringify(expenses));
+  }, [expenses]);
+
+  const balance = expenses.reduce((total, item) => {
+    return item.type === "income"
+      ? total + item.amount
+      : total - item.amount;
+  }, 0);
 
   return (
     <>
-     <Balance amount={balance} />
+      <Balance amount={balance} />
       <ExpenseForm onAddExpense={addExpense} />
-     <ExpenseList expenses={expenses} onToggleDone={toggleDone} onToggleDelete ={toggleDelete} />
-      
+      <ExpenseList
+        expenses={expenses}
+        onToggleDone={toggleDone}
+        onToggleDelete={toggleDelete}
+      />
     </>
   );
 }
